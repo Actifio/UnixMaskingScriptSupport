80e5f6dbc22c91c576be1bc7e768af834b4fe225
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2019-05-03 08:10:54 +1000

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+
+# the line run in the  mask function can be substituted for your own masking software.   In this example it calls a SQL script
+maskfunc()
+{
 su -m oracle -c "/act/scripts/masking.sh"
+}
+
+# this part of the script ensures we run the masking during a scrub mount after the database is started on the scrubbing server
+if [ "$ACT_MULTI_OPNAME" == "scrub-mount" ] && [ "$ACT_MULTI_END" == "true" ] && [ "$ACT_PHASE" == "post" ]; then
+	maskfunc
+	exit $?
+fi
+
+# this lets us run this script manually 
+if [ "$1" == "test" ]; then
+	maskfunc
+	exit $?
+fi
+
+exit 0
