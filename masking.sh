ece419d2902ddd758e259279546aa719944d5def
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-05-04 08:39:28 +1000

    Redundant File removal

--- masking.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# the three exported variables need to be modified to suit your implementation if using SQL script above.
-export ORACLE_HOME=/home/oracle/app/oracle/product/11.2.0/dbhome_1
-export PATH=/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/oracle/bin:$ORACLE_HOME/bin
-export ORACLE_SID=prepdmdb
-ORAENV_ASK=NO
-
-echo exit | sqlplus / as sysdba @maskscript.sql
