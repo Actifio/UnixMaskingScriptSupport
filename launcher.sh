ad9ee88f83df0a252ca84c16f24c47990a306f2f
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-05-04 08:44:42 +1000

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,13 +2,13 @@
 
 # the line run in the  mask function can be substituted for your own masking software.   In this example it calls a SQL script
 # You need to validate all the variables
-# Is your ORACLE_SID the one called by workflow?  In this example it is called:  unmasked
+# Is your ORACLE_SID the one called by workflow?  In this example it is called:  prepdmdb
 # Is your ORACLE_HOME correct?  In this example it is:  /home/oracle/app/oracle/product/12.2.0/dbhome_1
 # Is the SQL script being called the correct one?   In this example it is called: maskscript.sql
 
 maskfunc()
 {
-su - oracle -c "cd /act/scripts;export ORACLE_SID=unmasked;export ORACLE_HOME=/home/oracle/app/oracle/product/12.2.0/dbhome_1;export PATH=$ORACLE_HOME/bin:$PATH;ORAENV_ASK=NO;sqlplus / as sysdba @/act/scripts/maskscript.sql;exit"
+su - oracle -c "cd /act/scripts;export ORACLE_SID=prepdmdb;export ORACLE_HOME=/home/oracle/app/oracle/product/12.2.0/dbhome_1;export PATH=$ORACLE_HOME/bin:$PATH;ORAENV_ASK=NO;sqlplus / as sysdba @/act/scripts/maskscript.sql;exit"
 }
 
 # this part of the script ensures we run the masking during a liveclone scrub mount after the database is started on the scrubbing server
