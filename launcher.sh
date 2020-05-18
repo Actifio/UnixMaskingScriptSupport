52b1bf1c7e539e0fabfb8cce8c4e4c305d85e8dd
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-05-19 08:28:59 +1000

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# the line run in the  mask function can be substituted for your own masking software.   In this example it calls a SQL script
-# You need to validate all the variables
-# Is your ORACLE_SID the one called by workflow?  In this example it is called:  prepdmdb
-# Is your ORACLE_HOME correct?  In this example it is:  /home/oracle/app/oracle/product/12.2.0/dbhome_1
-# Is the SQL script being called the correct one?   In this example it is called: maskscript.sql
+# You don't need to set any variables unless you are manually running this script
+# ORACLE_SID is learned from env variable databasesid that exists during mount, but not manual test
+# ORACLE_HOME is learned from env variable orahome that exists only during certain parts of mount, but not manual test
+# sqlscriptname is learned by $2 passed to script during mount, but not manual test
+
+#  if $2 is not set then the sql script name will be empty
+if [ -z "$2" ]; then
+	echo "The $2 variable which defines the SQL script to be run was not passed to the script"
+	echo "In a workflow you need to put the SQL file name after the script name"
+else
+	sqlscriptname=$(echo "$2")
+fi
 
 maskfunc()
 {
-su - oracle -c 'cd /act/scripts;export ORACLE_SID=prepdmdb;export ORACLE_HOME=/home/oracle/app/oracle/product/12.2.0/dbhome_1;export PATH=$ORACLE_HOME/bin:$PATH;ORAENV_ASK=NO;sqlplus / as sysdba @/act/scripts/maskscript.sql;exit'
+ORACLE_HOME=$orahome
+ORACLE_SID=$databasesid
+oraclecommand="cd /act/scripts;export ORACLE_HOME=$ORACLE_HOME;export ORACLE_SID=$ORACLE_SID;export PATH=$ORACLE_HOME/bin:$PATH;ORAENV_ASK=NO;sqlplus / as sysdba @/act/scripts/$sqlscriptname;exit"
+echo "$oraclecommand"
+su -m oracle -c "$oraclecommand"
 }
 
 # this part of the script ensures we run the masking during a liveclone scrub mount after the database is started on the scrubbing server
